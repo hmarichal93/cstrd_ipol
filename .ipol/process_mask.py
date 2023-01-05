26f22acb9785ccb69d724afcda91b46ccd6ed571
henry <hmarichal93@gmail.com>
2023-01-05 13:01:04 -0300

    process run

--- .ipol/process_mask.py
+++ .ipol/process_mask.py
@@ -6,8 +6,8 @@ def main(filename):
     img = cv2.imread(filename)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     x,y = np.where(img>0)
-    print(f"Cy: {np.mean(y):.0f}")
-    print(f"Cx: {np.mean(x):.0f}")
+    print(f"{np.mean(y):.0f}")
+    print(f"{np.mean(x):.0f}")
     return 0
 
 if __name__=="__main__":
