67676e8f26ae653b5fe4b6d3ca5cc1198fa885bf
henry <hmarichal93@gmail.com>
2023-01-05 12:49:26 -0300

    process mask

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -16,7 +16,7 @@ HOME=$9
 if [ ! -f input_1.png ]; then
   convert mask_0.png -white-threshold 000001 -alpha off mask_0_black.png
   cp mask_0_black.png input_1.png
-  stdout=$(python $BIN/.ipol/process_mask.py)
+  stdout=$(python $BIN/.ipol/process_mask.py --input input_1.png)
   echo $stdout
 fi
 
