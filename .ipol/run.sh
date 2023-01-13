1fb7154512d5a4d83bfc855a097487ba7beaa2ac
henry <hmarichal93@gmail.com>
2023-01-13 16:49:54 -0300

    center add as point

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -13,13 +13,14 @@ BIN=$8
 HOME=$9
 
 # Extract center from mask
-if [ ! -f input_1.png ]; then
-  convert mask_0.png -white-threshold 000001 -alpha off mask_0_black.png
-  cp mask_0_black.png input_1.png
-  stdout=$(python $BIN/.ipol/process_mask.py --input input_1.png)
-  Cy=$(echo $stdout | awk '{print $1}')
-  Cx=$(echo $stdout | awk '{print $2}')
-fi
+#if [ ! -f input_1.png ]; then
+#  convert mask_0.png -white-threshold 000001 -alpha off mask_0_black.png
+#  cp mask_0_black.png input_1.png
+#  stdout=$(python $BIN/.ipol/process_mask.py --input input_1.png)
+#  Cy=$(echo $stdout | awk '{print $1}')
+#  Cx=$(echo $stdout | awk '{print $2}')
+#fi
+cat inpainting_data_0.txt
 
 # Execute algorithm
 python $BIN/main.py --input $input --cx $Cx --cy $Cy --root $BIN --output_dir ./ --nr $nr --th_high $th_high --th_low $th_low --hsize $hsize --wsize $wsize --sigma $sigma
