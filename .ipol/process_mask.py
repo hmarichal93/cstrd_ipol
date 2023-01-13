930ec8ef0b070d6f823c92361663632a5c74e4cf
henry <hmarichal93@gmail.com>
2023-01-13 16:58:45 -0300

    point mask

--- .ipol/process_mask.py
+++ .ipol/process_mask.py
@@ -3,11 +3,14 @@ import argparse
 import cv2
 
 def main(filename):
-    img = cv2.imread(filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    x,y = np.where(img>0)
-    print(f"{np.mean(y):.0f}")
-    print(f"{np.mean(x):.0f}")
+    # img = cv2.imread(filename)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # x,y = np.where(img>0)
+    f = open(filename, "r")
+    string = f.readlines()[0]
+    y, x = string.replace("[", "").replace("]", "").split(",")
+    print(f"{y}")
+    print(f"{x}")
     return 0
 
 if __name__=="__main__":
