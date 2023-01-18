306e404f3f27e8b4e3479844ab437038261433b1
henry <hmarichal93@gmail.com>
2023-01-18 18:14:56 -0300

    automatic center detection

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -13,14 +13,13 @@ BIN=$8
 HOME=$9
 
 # Extract center from mask
-cat inpainting_data_0.txt
 if [ -s inpainting_data_0.txt ]; then
-  echo 'manual'
+  # File is not empty
   stdout=$(python $BIN/.ipol/process_center.py --input inpainting_data_0.txt --type 0)
   rm inpainting_data_0.txt
 
 else
-  echo 'automatico'
+  # File is  empty
   $ANT_CENTER_DETECTOR/build/AntColonyPith --animated=false --input $input
   stdout=$(python $BIN/.ipol/process_center.py --input $input --type 1)
 
@@ -28,8 +27,6 @@ fi
 Cy=$(echo $stdout | awk '{print $1}')
 Cx=$(echo $stdout | awk '{print $2}')
 
-echo $Cy
-echo $Cx
 # Execute algorithm
-#python $BIN/main.py --input $input --cx $Cx --cy $Cy --root $BIN --output_dir ./ --nr $nr --th_high $th_high --th_low $th_low --hsize $hsize --wsize $wsize --sigma $sigma
+python $BIN/main.py --input $input --cx $Cx --cy $Cy --root $BIN --output_dir ./ --nr $nr --th_high $th_high --th_low $th_low --hsize $hsize --wsize $wsize --sigma $sigma
 
