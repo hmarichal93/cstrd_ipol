216fa00c26ca2740f8dd9cac2a2f7da8702891b2
henry <hmarichal93@gmail.com>
2023-01-18 18:26:48 -0300

    automatic center detection

--- .ipol/process_center.py
+++ .ipol/process_center.py
@@ -6,19 +6,15 @@ def mannually(filename):
     f = open(filename, "r")
     string = f.readlines()[0]
     y, x = string.replace("[", "").replace("]", "").split(",")
-    print(f"{int(y)}")
-    print(f"{int(x)}")
     f.close()
-    return 0
+    return y,x
 
 def automatic(filename):
     image_name, _ = filename.split(".")
     arr = np.loadtxt(f"{image_name}.csv",delimiter=",", dtype=str)
     y = arr[1][0]
     x = arr[1][1]
-    print(f"{int(y)}")
-    print(f"{int(x)}")
-    return 0
+    return y,x
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -26,6 +22,10 @@ if __name__=="__main__":
     parser.add_argument("--type", type=int, required=True, default=0)
     args = parser.parse_args()
     if args.type == 0:
-        mannually(args.input)
+        y,x = mannually(args.input)
     else:
-        automatic(args.input)
+        y,x = automatic(args.input)
+
+    print(f"{int(y)}")
+    print(f"{int(x)}")
+    return 0
