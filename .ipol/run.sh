95efb6b6ed51d2c872ebb3987b5ba4476fd3c608
henry <hmarichal93@gmail.com>
2023-01-18 17:29:47 -0300

    automatic center detection

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -17,7 +17,7 @@ if [ ! -f inpainting_data_0.txt ]; then
   stdout=$(python $BIN/.ipol/process_mask.py --input inpainting_data_0.txt)
 else
   $ANT_CENTER_DETECTOR/build/AntColonyPith --animated=false --input $input
-  stdout=$(python $BIN/.ipol/process_mask.py --input $input)
+  stdout=$(python $BIN/.ipol/process_mask.py --input $input --type 1)
   echo 'success'
 fi
 Cy=$(echo $stdout | awk '{print $1}')
