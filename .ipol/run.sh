332911ca312702240cbf4e2d9b0726b652dd3c89
henry <hmarichal93@gmail.com>
2023-03-04 10:43:26 -0300

    save image debugs

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -28,5 +28,5 @@ Cx=$(echo $stdout | awk '{print $1}')
 Cy=$(echo $stdout | awk '{print $2}')
 
 # Execute algorithm
-python $BIN/main.py --input $input --cx $Cx --cy $Cy --root $BIN --output_dir ./ --nr $nr --th_high $th_high --th_low $th_low --hsize $hsize --wsize $wsize --sigma $sigma
+python $BIN/main.py --input $input --cx $Cx --cy $Cy --root $BIN --output_dir ./ --nr $nr --th_high $th_high --th_low $th_low --hsize $hsize --wsize $wsize --sigma $sigma --save_imgs 1
 
