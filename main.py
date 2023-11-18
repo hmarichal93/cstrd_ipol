2efc6b640e06d73fbe3bbe33cc8cf3e38edbd949
henry <hmarichal93@gmail.com>
2023-11-18 19:04:17 -0300

    main refactoring

--- main.py
+++ main.py
@@ -50,23 +50,23 @@ def TreeRingDetection(im_in, cy, cx, sigma, th_low, th_high, height, width, alph
     """
     to = time.time()
 
-    # 1.0 Preprocessing image. Algorithm 2 in the paper. Image is  resized, converted to gray scale and equalized
+    # Line 1 Preprocessing image. Algorithm 2 in the paper. Image is  resized, converted to gray scale and equalized
     im_pre, cy, cx = preprocessing(im_in, height, width, cy, cx)
-    # 2.0 Edge detector module. Algorithm: A Sub-Pixel Edge Detector: an Implementation of the Canny/Devernay Algorithm,
+    # Line 2 Edge detector module. Algorithm: A Sub-Pixel Edge Detector: an Implementation of the Canny/Devernay Algorithm,
     m_ch_e, gx, gy = canny_deverney_edge_detector(im_pre, sigma, th_low, th_high)
-    # 3.0 Edge filtering module. Algorithm 5 in the paper.
+    # Line 3 Edge filtering module. Algorithm 5 in the paper.
     l_ch_f = filter_edges(m_ch_e, cy, cx, gx, gy, alpha, im_pre)
-    # 4.0 Sampling edges. Algorithm 7 in the paper
+    # Line 4 Sampling edges. Algorithm 7 in the paper
     l_ch_s, l_nodes_s = sampling_edges(l_ch_f, cy, cx, im_pre, mc, nr, debug=debug)
-    # 5.0 Connect chains. Algorithm 8 in the paper. Im_pre is used for debug purposes
+    # Line 5 Connect chains. Algorithm 8 in the paper. Im_pre is used for debug purposes
     l_ch_c,  l_nodes_c = connect_chains(l_ch_s, cy, cx, nr, debug, im_pre, debug_output_dir)
-    # 7.0 Postprocessing chains. Algorithm 19 in the paper. Im_pre is used for debug purposes
+    # Line 6 Postprocessing chains. Algorithm 19 in the paper. Im_pre is used for debug purposes
     l_ch_p = postprocessing(l_ch_c, l_nodes_c, debug, debug_output_dir, im_pre)
     # Line 7
     debug_execution_time = time.time() - to
     l_rings = chain_2_labelme_json(l_ch_p, height, width, cy, cx, im_in, debug_image_input_path, debug_execution_time)
 
-
+    # Line 8
     return im_in, im_pre, m_ch_e, l_ch_f, l_ch_s, l_ch_c, l_ch_p, l_rings
 
 
